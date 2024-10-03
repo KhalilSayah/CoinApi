0dc449558b9a130836c641d2698db301d2b08897
SayahKhalil <sayahkhalil2001@gmail.com>
2024-10-03 11:30:24 +0200

    finishing price CMC Scraper

--- scraper_module/scraper/items.py
+++ scraper_module/scraper/items.py
@@ -4,9 +4,34 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from typing import List
+from pydantic import BaseModel # type: ignore
 
 
-class ScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+
+class TokenPriceDetails(BaseModel):
+    price : float
+    volume : float
+    market_cap : float
+    price_in_btc : float
+    circulating_supply : float
+
+class PriceDataPoint(BaseModel):
+    timestamp : str
+    item: TokenPriceDetails
+
+class TokenPrice(BaseModel):
+    price_data : List[PriceDataPoint]
+
+class ApiResponseStatus(BaseModel):
+    timestamp : str
+    error_code : str
+    error_message : str
+    elapsed : str
+    credit_count : int
+class ApiResponseItem(scrapy.Item):
+    item_id = scrapy.Field()
+    token_info=scrapy.Field()
+    status = scrapy.Field()
+    data = scrapy.Field()
+
--- scraper_module/scraper/spiders/cmc_spider.py
+++ scraper_module/scraper/spiders/cmc_spider.py
@@ -1,4 +1,6 @@
+import json
 from scraper.scripts.get_map_tokens_cmc import get_maping, get_tokens_maping
+from ..items import ApiResponseItem, ApiResponseStatus, PriceDataPoint, TokenPrice, TokenPriceDetails
 import scrapy
 
 
@@ -15,8 +17,41 @@ class CMCSpider(scrapy.Spider):
                 callback=self.parse,
                 headers={
                     'User-Agent': self.custom_user_agent
-                }
+                },
+                meta={'token_info': token}
             )
 
     def parse(self, response):
-        yield response.json()
+        response_json = response.json()
+        token = response.meta['token_info']
+        response_status = ApiResponseStatus.parse_obj(response_json["status"])
+        data_points = response_json["data"]["points"]
+        price_data_points = []
+
+        for timestamp, point in data_points.items():
+            v_list = point['v']
+            token_price_details = TokenPriceDetails(
+                price=v_list[0],           # price
+                volume=v_list[1],          # volume
+                market_cap=v_list[2],      # market_cap
+                price_in_btc=v_list[3],    # price_in_btc
+                circulating_supply=v_list[4]  # circulating_supply
+            )
+
+            price_data_point = PriceDataPoint(
+                    timestamp=timestamp,
+                    item=token_price_details
+            )
+            price_data_points.append(price_data_point)
+
+        token_price = TokenPrice(
+            price_data=price_data_points
+        )
+
+
+        yield ApiResponseItem(
+            item_id=token.id,
+            token_info = token,
+            status =response_status,
+            data=token_price
+        )
